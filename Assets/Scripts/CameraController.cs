834c11a25aeae290d38b692e6a911dcb68b536e6
Nguyen Ba Quan <quanneprvt@gmail.com>
2019-08-06 17:46:45 +0700

    'update_src'

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float moveSpeed = 50f;
+    //
+    float hMove = 0f;
+    bool isJump = false;
+    bool isCrouch = false;
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        //
+        if (Input.GetButtonDown("Jump"))
+            isJump = true;
+        //
+        if (Input.GetButtonDown("Crouch"))
+            isCrouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            isCrouch = false;
+        //
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(hMove * Time.fixedDeltaTime, isCrouch, isJump);
+        if (isJump)
+            isJump = false;
+    }
+}
